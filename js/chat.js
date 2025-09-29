29be1246b8ffcec34940b53a75d62391089fe738
IRSwish <48881467+IRSwish@users.noreply.github.com>
2025-09-30 00:20:01 +0200

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,3 +1,14 @@
-export function loadChat(channel){
-    if(channel) document.getElementById("chatFrame").src=`https://www.twitch.tv/popout/${channel}/chat`;
+let chatEmbed = null;
+
+export function loadChat(channel) {
+  if (!channel) return;
+  document.getElementById("twitch-embed").innerHTML = ""; // reset
+
+  chatEmbed = new Twitch.Embed("twitch-embed", {
+    width: "100%",
+    height: "100%",
+    channel: channel,
+    layout: "chat", // juste le chat
+    theme: "dark"   // optionnel: dark ou light
+  });
 }
